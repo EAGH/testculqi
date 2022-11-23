c7c5803b13c3b9a7162ee86edb16bcdfd878b44c
Eduardo Gomez <eduardo.gomez.sk8@gmail.com>
2022-11-23 05:23:17 +0000

    LAST UPDATE

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # testculqi
-Pequeña practica de una funcion lambda en AWS
+Pequeña practica de una funcion lambda en AWS en nodejs:18
+
+Peticiones HTTP:
+GET: https://n7jp4dqyc6.execute-api.us-east-2.amazonaws.com/default/testculqi
+POST: https://n7jp4dqyc6.execute-api.us-east-2.amazonaws.com/default/newcard
+
+* Importe a su postman el archivo testculqi.postman_collection.json para ejecutar las peticiones.
+
+- Ejecutar en LAMBDA AWS comprima en un zip los siguientes ficheros (node_modules, package-lock.json, package.json, index.js) y carguelo en su LAMBDA FUNCTION.
+- Ejecutar en entorno local desde VSCODE utilice el paquete Dev Containers de docker.
--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-import { MongoClient } from "mongodb";
-const MONGODB_URI = "mongodb+srv://testculqi:testculqi@testculqi.ytvbzv3.mongodb.net/?retryWrites=true&w=majority";
+import { MongoClient, ObjectId } from "mongodb";
 let cachedDb = null;
 // funcion para conectar a la DB
 async function connectToDatabase() {
     if (cachedDb) {
         return cachedDb;
     }
-    const client = await MongoClient.connect(MONGODB_URI);
+    const client = await MongoClient.connect("mongodb+srv://testculqi:testculqi@testculqi.ytvbzv3.mongodb.net/?retryWrites=true&w=majority");
     const db = await client.db("testculqidb");
     cachedDb = db;
     return db;
 }
+
 // funcion principal de lambda
 export const handler = async (event) => {
     let response = {
         statusCode: 400,
         body: 'Error endpoint',
     };
-    // verificacion del token primeramente
-    if (!tokenMiddleware(event.headers.authorization)) return response;
     // si el token esta bien podria continuar con ejecutar la coneccion de la bd
     const db = await connectToDatabase();
-    //se obtiene el metodo http para verificar que sea post
-    const method = event.requestContext.http.method;
-    if (method === 'POST') {
+    //se obtiene el metodo http para verificar que sea post o get
+    if (event.requestContext.http?.method != undefined && event.requestContext.http.method === 'POST') {
         try {
             let body = JSON.parse(event.body)
+            // verificacion del token primeramente
+            if (tokenMiddleware(event.headers.authorization) === '') throw 'Error in token';
             if (!valid_credit_card(body.card_number)) throw 'Error Card Number' // validate LUHN
             if (!valid_length(body.card_number, 13) && !valid_length(body.card_number, 16)) throw 'Error length Card Number' // validate cards
             if (!valid_length(body.cvv, 3) && !valid_length(body.cvv, 4)) throw 'Error length Code CVV' // validate cvv
             if ((!valid_length(body.expiration_month, 1) && !valid_length(body.expiration_month, 2)) || !valid_month(body.expiration_month)) throw 'Error length month' // validate month
             if (!valid_year(body.expiration_year) || !valid_length(body.expiration_year, 4)) throw 'Error year' // validate year
             if (body.email.length < 5 || body.email.length > 100 || !valid_email(body.email)) throw 'Error email' // validate email
-            const cards = await db.collection("cards").insertOne(body);
+            const getNewId = new ObjectId();
+            const newToken = generateId()+getNewId.toString().slice(-6);
+            const dataInsert = {
+                "_id": getNewId,
+                "token": newToken,
+                "email": body.email,
+                "card_number": body.card_number,
+                "cvv": body.cvv,
+                "expiration_year": body.expiration_year,
+                "expiration_month": body.expiration_month,
+                "createdAt": new Date(),
+            }
+            await db.collection("cards").createIndex( { "createdAt": 1 }, { expireAfterSeconds: 900 } )
+            await db.collection("cards").insertOne(dataInsert);
             response = {
                 statusCode: 200,
-                body: JSON.stringify(cards),
+                body: newToken,
             };
         } catch (error) {
             response = {
@@ -44,11 +56,29 @@ export const handler = async (event) => {
             };
         }
         return response;
-    }else if (method === 'GET'){
-        
+    } else if (event.httpMethod != undefined && event.httpMethod === 'GET') {
+        try {
+            // verificacion del token primeramente
+            const tokenData = tokenMiddleware(event.headers.authorization);
+            if (tokenData === '') throw 'Error in token';
+            const options = { projection: { _id: 0, email: 1, card_number: 1, expiration_year: 1, expiration_month: 1 } };
+            const cardData = await db.collection("cards").findOne({ token: tokenData }, options);
+            if (cardData == null)  throw 'The card does not exist';
+            response = {
+                statusCode: 200,
+                body: JSON.stringify(cardData),
+            };
+        } catch (error) {
+            response = {
+                statusCode: 400,
+                body: error,
+            };
+        }
+        return response;
     }
 };
-// funciones validadores con nombres que se definen ellos mimos
+
+// funciones validadores con nombres que se definen ellos mismos
 function valid_credit_card(value) {
     if (/[^0-9-\s]+/.test(value)) return false;
     var nCheck = 0, nDigit = 0, bEven = false;
@@ -87,6 +117,15 @@ function valid_email(value) {
 }
 function tokenMiddleware(value) {
     const token = value.split(" ")[1];
-    if (token.length == 24 && token.split("_")[0] == "pk" && token.split("_")[1] == "test") return true;
-    return false;
+    if (token.split("_")[2].length == 16 && token.split("_")[0] == "pk" && token.split("_")[1] == "test") return token.split("_")[2];
+    return '';
+}
+function generateId() {
+    var result = '';
+    var characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 10; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
